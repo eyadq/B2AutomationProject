c6bffa742ebc5027edfc5291c30cf38f0a8860f2
Eyad Quraan <EyadQuraan@gmail.com>
2023-11-26 15:14:43 -0500

    day 3 locator practice, getTitle(), getCurrentURL(), etc

---
+++ src/test/java/com/loop/test/day4_xpath_findelement/T00_css_isDisplayed.java
@@ -0,0 +1,29 @@
+package com.loop.test.day4_xpath_findelement;
+
+import com.loop.test.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T00_css_isDisplayed {
+
+     /*
+    http://the-internet.herokuapp.com/forgot_password
+     */
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://the-internet.herokuapp.com/forgot_password");
+
+        WebElement forgotPasswordHeading = driver.findElement(By.cssSelector("div[class='example']>h2"));
+
+        if(forgotPasswordHeading.isDisplayed()){
+            System.out.println("Forgot password heading is displayed");
+        } else {
+            System.out.println("Forgot password heading is NOT displayed");
+        }
+
+    }
+}
