1c21b2a62af875901fba105fb72010a88262903d
Eyad Quraan <EyadQuraan@gmail.com>
2023-12-21 19:00:06 -0500

    day8 Web tables

--- src/test/java/com/loop/test/day7_windows/T00_login_docuport.java
+++ src/test/java/com/loop/test/day7_windows/T00_login_docuport.java
@@ -3,14 +3,32 @@ package com.loop.test.day7_windows;
 import com.loop.test.base.TestBase;
 import com.loop.test.utilities.DocuportConstants;
 import com.loop.test.utilities.DocuportUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class T00_login_docuport extends TestBase {
 
     @Test
     public void test_login_logout_docuport() throws InterruptedException {
         DocuportUtils.login(driver, DocuportConstants.CLIENT);
-        DocuportUtils.logout(driver);
+        DocuportUtils.logOut(driver);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(d-> driver.findElement(By.xpath("//label[text()='Username or email']")));
+
+        DocuportUtils.login(driver, DocuportConstants.ADVISOR);
+        wait.until(d-> driver.findElement(By.xpath("//div[@class='v-avatar primary']")));
+        DocuportUtils.logOut(driver);
+
+        wait.until(d-> driver.findElement(By.xpath("//label[text()='Username or email']")));
+        DocuportUtils.login(driver, DocuportConstants.EMPLOYEE);
+        DocuportUtils.logOut(driver);
+
+        DocuportUtils.login(driver, DocuportConstants.SUPERVISOR);
+        DocuportUtils.logOut(driver);
     }
 
 
